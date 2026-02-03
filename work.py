9be10976d9b16e47d80a13fa3a721951c28e6108
VictorYXL <1601210807@pku.edu.cn>
2026-02-03 08:10:01 +0000

    abstain

--- work.py
+++
@@ -1,4 +0,0 @@
-import os
-results = os.path.join("output", "Arxiv.AzureGPT4o", "results.json")
-output_file1 = os.path.join("output", "Arxiv.AzureGPT4o", "prediction.json")
-output_file2 = os.path.join("output", "Arxiv.Qwen3-8B", "results.json")
